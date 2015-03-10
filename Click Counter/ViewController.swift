573c6d334ca5ae23f91651c01900802049c3b5dc
avi <avinashsajjan@gmail.com>
2015-03-10 18:08:53 +0530

    added a button using only code

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -14,10 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // adds a label
         var label = UILabel()
         label.frame = CGRectMake(150, 150, 60, 60)
-        label.text = "Hello!"
+        label.text = "0"
         view.addSubview(label)
+        
+        // adds a button
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
